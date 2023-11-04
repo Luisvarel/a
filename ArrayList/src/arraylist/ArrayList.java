4fc8913d60ed6795775f2abead75b4bb0834a9e3
Luisvarel <132074260+Luisvarel@users.noreply.github.com>
2023-11-03 19:47:58 -0600

    Add files via upload

---
+++ ArrayList/src/arraylist/ArrayList.java
@@ -0,0 +1,27 @@
+package arraylist;
+import java.util.*;
+
+
+
+public class ArrayList {
+
+    /*
+    *ArrayList
+    arreglo mejorado
+    *Crece Dinamicamente
+    *Cuenta con todos los metodos que necesita 
+    para administrar 
+    
+     */
+    /*
+    *Casteo
+    tipo declarado=((tipo declarado)tipo ins)
+    */
+    /*
+    ArrayList lista=new ArrayList();//No tipado
+    ArrayList <Integer>li=new ArrayList();//tipado
+    */
+
+    public static void main(String[] args) {
+    }
+}
